b092462a9694b2a8ccfc9e4f7cda642d62f0fe80
Sulaiman Sulaman <sulaimansulaman@gmail.com>
2025-08-21 15:35:07 +0200

    Horizontal Adjustments
    Horizontal Adjustments

--- arcademia-blockly-/Main.gd
+++ arcademia-blockly-/Main.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-@onready var webview: Control = $HBoxContainer/WebView
-@onready var game_area: Control = $HBoxContainer/GameArea
-@onready var player_node: Node2D = $HBoxContainer/GameArea/World/Player
+@onready var webview: Control = $VBoxContainer/WebView
+@onready var game_area: Control = $VBoxContainer/GameArea
+@onready var player_node: Node2D = $VBoxContainer/GameArea/World/Player
 
 var player_target: Vector2
 
