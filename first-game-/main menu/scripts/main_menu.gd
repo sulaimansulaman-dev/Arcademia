86d2c82bff794c363f937cd9d684b4190757d2a7
Sulaiman Sulaman <sulaimansulaman@gmail.com>
2025-09-12 18:10:49 +0200

    Main Menu Linking Levels blocks

--- first-game-/main menu/scripts/main_menu.gd
+++ first-game-/main menu/scripts/main_menu.gd
@@ -1,17 +1,26 @@
 extends Control  # or Node if MainMenu is not UI-based
 
 func _on_level_1_pressed() -> void:
-	get_tree().change_scene_to_file("res://game/scenes/Blocks.tscn")
 	Globals.level_to_load = 1
+	get_tree().change_scene_to_file("res://game/scenes/Blocks.tscn")
 
 func _on_level_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://game/scenes/Level 2.tscn")
 	Globals.level_to_load = 2
+	get_tree().change_scene_to_file("res://game/scenes/Blocks.tscn")
+	
+func _on_level_3_pressed() -> void:
+	Globals.level_to_load = 3
+	get_tree().change_scene_to_file("res://game/scenes/Blocks.tscn")
+	
+func _on_level_4_pressed() -> void:
+	Globals.level_to_load = 4
+	get_tree().change_scene_to_file("res://game/scenes/Blocks.tscn")
+	
 
+	
+func _on_avatar_pressed() -> void:
+	get_tree().change_scene_to_file("res://avatar creation/scenes/control.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 	
-	
-func _on_avatar_pressed() -> void:
-	get_tree().change_scene_to_file("res://avatar creation/scenes/control.tscn")
