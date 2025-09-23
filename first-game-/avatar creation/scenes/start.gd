0a3a18e447f98a94e4dc8091e01180277ba8ee77
Mbuyelo <mbowenimb27@gmail.com>
2025-09-23 19:20:24 +0200

    Fixed Order or menus

--- first-game-/avatar creation/scenes/start.gd
+++ first-game-/avatar creation/scenes/start.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_student_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://avatar creation/scenes/StudentEntry.tscn")
+	get_tree().change_scene_to_file("res://avatar creation/scenes/control.tscn")
 
 
 func _on_teacher_button_pressed() -> void:
