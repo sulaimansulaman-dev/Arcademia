4dc3afbebcbeba85f1884b8e3abe82d156c9094a
Mbuyelo <mbowenimb27@gmail.com>
2025-09-26 15:50:01 +0200

    Merging Sulis, and Natashas branch

---
+++ first-game-/avatar creation/Natashas_Stuff/main menu/scenes/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+func _on_level_1_pressed() -> void:
+	Globals.level_to_load = 1
+	get_tree().change_scene_to_file("res://game/scenes/Blocks.tscn")
+
+
+func _on_level_2_pressed() -> void:
+	Globals.level_to_load = 2
+	get_tree().change_scene_to_file("res://game/scenes/Blocks.tscn")
+
+
+func _on_level_3_pressed() -> void:
+	Globals.level_to_load = 3
+	get_tree().change_scene_to_file("res://game/scenes/Blocks.tscn")
+
+
+func _on_final_level_pressed() -> void:
+	Globals.level_to_load = 4
+	get_tree().change_scene_to_file("res://game/scenes/Blocks.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
