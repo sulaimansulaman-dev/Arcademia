fc5ad7c373568edf01851b04a704ac12e3f55b9c
Sulaiman Sulaman <sulaimansulaman@gmail.com>
2025-09-26 19:26:16 +0200

    Add To Group While Loop

--- first-game-/game/scripts/coin.gd
+++ first-game-/game/scripts/coin.gd
@@ -3,6 +3,8 @@ extends Area2D
 @onready var game_manager: Node = %GameManager
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	add_to_group("spaceship_part") 
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
